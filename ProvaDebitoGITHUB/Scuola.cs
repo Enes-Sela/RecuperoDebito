8e4a8e848dd706aa71ce455d90c8bd7e694dd0ad
Enes Sela <ENES.SELA@local.ittcesena.it>
2023-03-16 09:03:47 +0100

    Completo BranchScuola

--- ProvaDebitoGITHUB/Scuola.cs
+++ ProvaDebitoGITHUB/Scuola.cs
@@ -8,5 +8,46 @@ namespace ProvaDebitoGITHUB
 {
     internal class Scuola
     {
+        List<BranchAlunno> elencoAlunni = new List<BranchAlunno>();
+
+        internal string NomeScuola { get; set; }
+        internal int OrarioApertura { get; set; }
+        internal int OrarioChiusura { get; set; }
+
+        public void AggiuntaAlunno() 
+        {
+            elencoAlunni.Add(new BranchAlunno());
+        }
+
+        public void RicercaAlunniCognome(BranchAlunno cognome) 
+        {
+            for (int i = 0; i < elencoAlunni.Count; i++)
+            {
+                if (elencoAlunni.Contains(cognome))
+                {
+                    cognome = elencoAlunni[i];
+                }
+            }    
+        }
+
+        public void RicercaAlunniEtà(BranchAlunno alunno) 
+        {
+            for (int i = 0; i < elencoAlunni.Count; i++)
+            {
+                if (elencoAlunni.Contains(alunno))
+                {
+                    alunno.EtàAlunno = elencoAlunni.Count();    
+                }
+            }
+        }
+
+        public void TotaleAlunni() 
+        {
+            for (int i = 0; i < elencoAlunni.Count; i++)
+            {
+                int totaleAlunni = 0;
+                totaleAlunni = elencoAlunni.Count;
+            }
+        }
     }
 }
