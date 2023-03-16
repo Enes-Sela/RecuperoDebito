44de607d5fa6c9bc7e6147a6af3e4da6ec298fb4
Enes Sela <ENES.SELA@local.ittcesena.it>
2023-03-16 09:43:14 +0100

    Inizio BranchMaster

--- ProvaDebitoGITHUB/MainWindow.xaml.cs
+++ ProvaDebitoGITHUB/MainWindow.xaml.cs
@@ -24,5 +24,53 @@ namespace ProvaDebitoGITHUB
         {
             InitializeComponent();
         }
+
+        List<BranchAlunno> elencoAlunni = new List<BranchAlunno>(1000);
+
+        private void NuovoAlunno(object sender, RoutedEventArgs e)
+        {
+            BranchAlunno alunno1 = new BranchAlunno();
+
+            nome.Text = alunno1.NomeAlunno;
+            cognome.Text = alunno1.CognomeAlunno;
+            età.Text = alunno1.EtàAlunno.ToString();
+            cittàResidenza.Text = alunno1.CittàDiResidenza;
+
+            alunno1.ToString();
+        }
+
+        private void AggiuntaAlunno(object sender, RoutedEventArgs e)
+        {
+            BranchAlunno alunno2 = new BranchAlunno();
+
+            nome.Text = alunno2.NomeAlunno;
+            cognome.Text = alunno2.CognomeAlunno;
+            età.Text = alunno2.EtàAlunno.ToString();
+            cittàResidenza.Text = alunno2.CittàDiResidenza;
+
+            Scuola s1 = new Scuola();
+            s1.AggiuntaAlunno();
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            BranchAlunno alunno3 = new BranchAlunno();
+
+            età.Text = alunno3.EtàAlunno.ToString();
+
+            Scuola s1 = new Scuola();
+            s1.RicercaAlunniEtà(alunno3);
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < elencoAlunni.Count; i++)
+            {
+                alunnitotali.Text = elencoAlunni.Count.ToString();
+            }
+        }
     }
 }
